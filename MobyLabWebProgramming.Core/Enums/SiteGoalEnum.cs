728f9cf0dc574515aaae1fe6afd4a35a4354d2b4
Ovidiu Stoie <ovidiustoie@gmail.com>
2024-05-05 11:52:40 +0200

    bf

--- MobyLabWebProgramming.Core/Enums/SiteGoalEnum.cs
+++ MobyLabWebProgramming.Core/Enums/SiteGoalEnum.cs
@@ -4,11 +4,11 @@ using System.Text.Json.Serialization;
 
 namespace MobyLabWebProgramming.Core.Enums;
 
-[JsonConverter(typeof(SmartEnumNameConverter<SiteDifficultyEnum, string>))]
+[JsonConverter(typeof(SmartEnumNameConverter<SiteGoalEnum, string>))]
 public sealed class SiteGoalEnum : SmartEnum<SiteGoalEnum, string>
 {
     public static readonly SiteGoalEnum Yes = new(nameof(Yes), "Yes");
-    public static readonly SiteGoalEnum Partially = new(nameof(Partially), "Easy");
+    public static readonly SiteGoalEnum Partially = new(nameof(Partially), "Partially");
     public static readonly SiteGoalEnum No = new(nameof(No), "No");
    
     private SiteGoalEnum(string name, string value) : base(name, value)
